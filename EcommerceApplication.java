97239470d5e4ae56cec47b315e7a4212aa1c3824
umut.durmus <129973069+umut-durmus@users.noreply.github.com>
2023-05-07 11:49:06 +0300

    Add files via upload

---
+++ EcommerceApplication.java
@@ -0,0 +1,28 @@
+package com.etiya.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+
+
+	}
+
+
+
+
+
+
+
+
+}
